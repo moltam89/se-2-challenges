e0cda11536d11f5cd11fe7ca940214c08b6e1cd4
moltam89 <moltam89@gmail.com>
2024-05-07 16:29:55 +0200

    More tests

--- packages/hardhat/test/ChallengeN.ts
+++ packages/hardhat/test/ChallengeN.ts
@@ -13,6 +13,7 @@
 
 import { ethers } from "hardhat";
 import { DODOFlashloanArb } from "../typechain-types/DODOFlashloanArb.sol/DODOFlashloanArb";
+import { IERC20 } from "../typechain-types/DODOFlashloanArb.sol/IERC20";
 import { expect } from "chai";
 
 const addressUSDT = "0xdAC17F958D2ee523a2206206994597C13D831ec7"; 
@@ -27,27 +28,19 @@ const addressOneInchAggregator = "0x1111111254EEB25477B68fb85Ed929f73A960582";
 
 // These are the original transactions calldata for the swaps, contains the deployed contract address, 
 // we need to replace it with the address of our contract
-// https://etherscan.io/address/0xa951c184cf0c0f957468a9ab8ec3f76bc75262eb
 const calldataSwap1 = "0x12aa3caf0000000000000000000000003208684f96458c540eb08f6f01b9e9afb2b7d4f0000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec7000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb480000000000000000000000003208684f96458c540eb08f6f01b9e9afb2b7d4f0000000000000000000000000a951c184cf0c0f957468a9ab8ec3f76bc75262eb0000000000000000000000000000000000000000000000000000005d21dba0000000000000000000000000000000000000000000000000000000005d02190d97000000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000001400000000000000000000000000000000000000000000000000000000000000160000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000fc0000000000000000000000000000000000000000000000000000de0000b05120c9f93163c99695c6526b799ebca2207fdf7d61addac17f958d2ee523a2206206994597c13d831ec700048dae733300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005c2b97ac9a0000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000000080a06c4eca27a0b86991c6218b36c1d19d4a2e9eb0ce3606eb481111111254eeb25477b68fb85ed929f73a960582000000008b1ccac8";
 const calldataSwap2 = "0x12aa3caf0000000000000000000000003208684f96458c540eb08f6f01b9e9afb2b7d4f0000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec70000000000000000000000003208684f96458c540eb08f6f01b9e9afb2b7d4f0000000000000000000000000a951c184cf0c0f957468a9ab8ec3f76bc75262eb0000000000000000000000000000000000000000000000000000005d19ed03850000000000000000000000000000000000000000000000000000005d0b4f22f0000000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000001400000000000000000000000000000000000000000000000000000000000000160000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002d40000000000000000000000000000000000000000000002b600028800026e00a0c9e75c480000000000000000300200000000000000000000000000000000000000000000000000024000013051004a585e0f7c18e2c414221d6402652d5e0990e5f8a0b86991c6218b36c1d19d4a2e9eb0ce3606eb4800a4a5dcbcdf000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec7000000000000000000000000d0b2f5018b5d22759724af6d4281ac0b132663600000000000000000000000003208684f96458c540eb08f6f01b9e9afb2b7d4f0ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005120d17b3c9784510e33cd5b87b490e79253bcd81e2ea0b86991c6218b36c1d19d4a2e9eb0ce3606eb48004458d30ac9000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec700000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005884993bdd00000000000000000000000000000000000000000000000000000000000000000000000000000000000000003208684f96458c540eb08f6f01b9e9afb2b7d4f0000000000000000000000000000000000000000000000000000000006506b67a0020d6bdbf78dac17f958d2ee523a2206206994597c13d831ec780a06c4eca27dac17f958d2ee523a2206206994597c13d831ec71111111254eeb25477b68fb85ed929f73a9605820000000000000000000000008b1ccac8";
 const addressOriginalDODOFlashloanArb = "0xa951c184cf0c0f957468a9ab8ec3f76bc75262eb"; 
-// Helper method to replace the address of the original contract with the address of our contract
-function replaceCalldataAddress(calldata: string, addressOriginalReceiver: string, addressReceiver: string): string {
-  const addressOriginalReceiverInCalldata = addressOriginalReceiver.slice(2).toLowerCase();
-  const addressReceiverInCalldata = addressReceiver.slice(2).toLowerCase();
-
-  return calldata.replace(addressOriginalReceiverInCalldata, addressReceiverInCalldata);
-}
 
 // This is the calldata to approve 1inch to spend ERC20 tokens
 const oneInchApproveData = "0x095ea7b30000000000000000000000001111111254eeb25477b68fb85ed929f73a960582ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
 
 const flashLoanAmountUSDT = "400000000000"; // 400.000 USDT
 
-describe("🚩 Challenge N: Description", function () {
+describe("🚩 Challenge Arbitrage", function () {
   let dodoFlashloanArb: DODOFlashloanArb;
 
-  describe("DODOFlashloanArb", function () {
+  describe("Executing flahsloan aritrage, mimicing https://etherscan.io/address/0xa951c184cf0c0f957468a9ab8ec3f76bc75262eb ", function () {
     const contractAddress = process.env.CONTRACT_ADDRESS;
 
     // Don't change contractArtifact creation
@@ -59,11 +52,19 @@ describe("🚩 Challenge N: Description", function () {
       contractArtifact = "contracts/DODOFlashloanArb.sol:DODOFlashloanArb";
     }
 
-    it("Executing flahsloan aritrage ", async function () {
-      // Get the current block number
+    it("We are back in time around block number 18119016 where this arbitrage opportunity happened", async function () {
       const blockNumber = await ethers.provider.getBlockNumber();
       console.log("Current block number:", blockNumber);
+    });
 
+    it("DODO pool has more than 400.000", async function () {
+      const usdtContract = await ethers.getContractAt("IERC20", addressUSDT);
+      const balanceUSDTDODO = await usdtContract.balanceOf(addressDODOPool);
+      console.log("balance USDT DODO pool:", balanceUSDTDODO.toString());
+      expect(balanceUSDTDODO).to.be.gt(ethers.parseUnits("400000", 6));
+    });
+
+    it("We are back in time before block number 18119016 where this arbitrage opportunity happened", async function () {
       const dodoFlashloanArbFactory = await ethers.getContractFactory(contractArtifact);
       dodoFlashloanArb = (await dodoFlashloanArbFactory.deploy()) as DODOFlashloanArb;
 
@@ -71,6 +72,16 @@ describe("🚩 Challenge N: Description", function () {
 
       await dodoFlashloanArb.anyThing(addressUSDT, 0, oneInchApproveData);
       await dodoFlashloanArb.anyThing(addressUSDC, 0, oneInchApproveData);
+    });
+
+      it("We are back in time before block number 18119016 where this arbitrage opportunity happened", async function () {
+        const dodoFlashloanArbFactory = await ethers.getContractFactory(contractArtifact);
+        dodoFlashloanArb = (await dodoFlashloanArbFactory.deploy()) as DODOFlashloanArb;
+  
+        const addressDODOFlashloanArb = await dodoFlashloanArb.getAddress();
+  
+        await dodoFlashloanArb.anyThing(addressUSDT, 0, oneInchApproveData);
+        await dodoFlashloanArb.anyThing(addressUSDC, 0, oneInchApproveData);
 
 
       await dodoFlashloanArb.dodoFlashLoan(
@@ -87,3 +98,11 @@ describe("🚩 Challenge N: Description", function () {
     });
   });
 });
+
+// Helper method to replace the address of the original contract with the address of our contract
+function replaceCalldataAddress(calldata: string, addressOriginalReceiver: string, addressReceiver: string): string {
+  const addressOriginalReceiverInCalldata = addressOriginalReceiver.slice(2).toLowerCase();
+  const addressReceiverInCalldata = addressReceiver.slice(2).toLowerCase();
+
+  return calldata.replace(addressOriginalReceiverInCalldata, addressReceiverInCalldata);
+}
