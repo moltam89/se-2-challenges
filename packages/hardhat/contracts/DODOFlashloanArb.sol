8b6fff835bab461e8f0144a0907d7249d03236b1
moltam89 <moltam89@gmail.com>
2024-05-07 15:55:25 +0200

    Comments

--- packages/hardhat/contracts/DODOFlashloanArb.sol
+++ packages/hardhat/contracts/DODOFlashloanArb.sol
@@ -3,44 +3,35 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "hardhat/console.sol";
 
+// DODO flashoal 
+// https://docs.dodoex.io/en/developer/contracts/dodo-v1-v2/guides/flash-loan
 interface IDODO {
     function flashLoan(
-        uint256 baseAmount,
-        uint256 quoteAmount,
-        address assetTo,
-        bytes calldata data
+        uint256 baseAmount,      // Token 1 amount to borrow
+        uint256 quoteAmount,     // Token 2 amount to borrow
+        address assetTo,         // Address to send the borrowed tokens to
+        bytes calldata data      // Data to pass to the callback function
     ) external;
 }
 
 interface IERC20 {
     function transfer(address to, uint256 value) external;
-
     function balanceOf(address account) external view returns (uint256);
 }
 
 contract DODOFlashloanArb {
     address public constant addressUSDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
     address public constant addressUSDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-
-    function anyThing(
-        address to,
-        uint256 value,
-        bytes calldata data
-    ) public {
-        (bool success, ) = to.call{value: value}(data);
-
-        require(success, "CALL_FAILED");
-    }
     
     function dodoFlashLoan(
-        address flashLoanPool,
-        uint256 loanAmount,
-        address loanToken,
-        bool getFirstToken,
-        address aggregator1,
-        bytes memory data1,
-        address aggregator2,
-        bytes memory data2
+        address flashLoanPool,              // Address of the DODO USDT/DAI pool https://etherscan.io/address/0x3058ef90929cb8180174d74c507176cca6835d73#code
+        uint256 loanAmount,                 // 400.000 
+        address loanToken,                  // USDT address
+        bool getFirstToken,                 // Borrow USDT or DAI, we'll borrow USDT
+        address aggregator1,                // 1inch aggregator address
+        bytes memory data1,                 // 1inch swap data 1
+        address aggregator2,                // 1inch aggregator address (we could use other aggregators)
+        bytes memory data2                  // 1inch swap data 2
     ) external {
         bytes memory data = abi.encode(
             flashLoanPool,
@@ -52,12 +43,17 @@ contract DODOFlashloanArb {
             data2
         );
 
+
+        // Borrow token from DODO, send data for the callback function will be executed after the flashloan
         if (getFirstToken) {
             IDODO(flashLoanPool).flashLoan(loanAmount, 0, address(this), data);
         } else {
             IDODO(flashLoanPool).flashLoan(0, loanAmount, address(this), data);
         }
     }
+
+    // Different DODO pools have different callback functions
+    // All of these will call the same internal _flashLoanCallBack function
 
     //Note: CallBack function executed by DODOV2(DVM) flashLoan pool
     function DVMFlashLoanCall(
@@ -68,7 +64,6 @@ contract DODOFlashloanArb {
     ) external {
         _flashLoanCallBack(sender, baseAmount, quoteAmount, data);
     }
-
     //Note: CallBack function executed by DODOV2(DPP) flashLoan pool
     function DPPFlashLoanCall(
         address sender,
@@ -78,7 +73,6 @@ contract DODOFlashloanArb {
     ) external {
         _flashLoanCallBack(sender, baseAmount, quoteAmount, data);
     }
-
     //Note: CallBack function executed by DODOV2(DSP) flashLoan pool
     function DSPFlashLoanCall(
         address sender,
@@ -89,6 +83,8 @@ contract DODOFlashloanArb {
         _flashLoanCallBack(sender, baseAmount, quoteAmount, data);
     }
 
+    // This is the callback function that will be executed after the flashloan
+    // Here we should have the 400.000 USDT in our contract
     function _flashLoanCallBack(
         address sender,
         uint256,
@@ -108,33 +104,52 @@ contract DODOFlashloanArb {
                 (address, uint256, address, address, bytes, address, bytes)
             );
 
-        
-
+        // Make sure that the call is coming from the DODO pool
         require(
             sender == address(this) && msg.sender == flashLoanPool,
             "HANDLE_FLASH_NENIED"
         );
 
-        console.log("block.number", block.number);
+        getAndLogERC20Balance(addressUSDT, address(this), "USDT balance before swaps");
+        getAndLogERC20Balance(addressUSDC, address(this), "USDC balance before swaps");
 
         (bool success1, ) = aggregator1.call(data1);
         require(success1, "Swap 1 was not successfull");
 
-        getERC20Balance(addressUSDT, flashLoanPool);
-        getERC20Balance(addressUSDT, address(this));
-        getERC20Balance(addressUSDC, address(this));
+        getAndLogERC20Balance(addressUSDT, address(this), "USDT balance after swap1");
+        getAndLogERC20Balance(addressUSDC, address(this), "USDC balance after swap1");
 
         (bool success2, ) = aggregator2.call(data2);
         require(success2, "Swap 2 was not successfull");
 
+        getAndLogERC20Balance(addressUSDT, address(this), "USDT balance after swap2");
+        getAndLogERC20Balance(addressUSDC, address(this), "USDC balance after swap2");
+
         //Return funds
         IERC20(loanToken).transfer(flashLoanPool, loanAmount);
+
+        getAndLogERC20Balance(addressUSDT, address(this), "USDT balance after returning the loan");
+        getAndLogERC20Balance(addressUSDC, address(this), "USDC balance after returning the loan");
     }
 
-    function getERC20Balance(address erc20TokenAddress, address balanceAddress) public view returns (uint256) {
+    // This is a helper to call arbitrary functions on a contract
+    // We will use this to approve the 1inch contract to spend our USDT/USDC
+    // Could be used to get the USDT profit out from the contract (Should be guarded with an owner modifier)
+    function anyThing(
+        address to,
+        uint256 value,
+        bytes calldata data
+    ) public {
+        (bool success, ) = to.call{value: value}(data);
+
+        require(success, "CALL_FAILED");
+    }
+
+    // Log the balance of an ERC20 token
+    function getAndLogERC20Balance(address erc20TokenAddress, address balanceAddress, string memory state) public view returns (uint256) {
         uint256 balance = IERC20(erc20TokenAddress).balanceOf(balanceAddress);
 
-        console.log( erc20TokenAddress, balanceAddress, balance);
+        console.log(state, balance);
 
         return balance;
     }
--- packages/hardhat/test/ChallengeN.ts
+++ packages/hardhat/test/ChallengeN.ts
@@ -2,6 +2,15 @@
 // This script executes when you run 'yarn test'
 //
 
+// Script to mimic flashloan arbitrage using DODO and 1inch
+// https://etherscan.io/tx/0x60de3cd915a47882dff0d918694d4dcad133807e223e34d5c442b3a89f3e7a34
+// https://eigenphi.io/mev/eigentx/multi/0x60de3cd915a47882dff0d918694d4dcad133807e223e34d5c442b3a89f3e7a34
+
+// Borrow 400.000 USDT from DODO
+// Swap USDT for 399.867 USDC using 1inch 
+// Swap USDC for 400.021 USDT using 1inch
+// Tx cost 0.00758535552 * $1,593.01 / ETH = $12.07
+
 import { ethers } from "hardhat";
 import { DODOFlashloanArb } from "../typechain-types/DODOFlashloanArb.sol/DODOFlashloanArb";
 import { expect } from "chai";
@@ -9,16 +18,20 @@ import { expect } from "chai";
 const addressUSDT = "0xdAC17F958D2ee523a2206206994597C13D831ec7"; 
 const addressUSDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"; 
 
+// DODO USDT/DAI Pool, we will use this pool to perform the flashloan, borrowing 400.000 USDT, DODO has no fees for flashloans
 const addressDODOPool = "0x3058EF90929cb8180174D74C507176ccA6835D73";
+
+// 1inch Aggregator, we can send calldata directly to the 1inch contract to perform the swap
+// https://docs.1inch.io/docs/aggregation-protocol/api/swagger
 const addressOneInchAggregator = "0x1111111254EEB25477B68fb85Ed929f73A960582";
 
-const addressOriginalDODOFlashloanArb = "0xa951c184cf0c0f957468a9ab8ec3f76bc75262eb"; 
-
+// These are the original transactions calldata for the swaps, contains the deployed contract address, 
+// we need to replace it with the address of our contract
+// https://etherscan.io/address/0xa951c184cf0c0f957468a9ab8ec3f76bc75262eb
 const calldataSwap1 = "0x12aa3caf0000000000000000000000003208684f96458c540eb08f6f01b9e9afb2b7d4f0000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec7000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb480000000000000000000000003208684f96458c540eb08f6f01b9e9afb2b7d4f0000000000000000000000000a951c184cf0c0f957468a9ab8ec3f76bc75262eb0000000000000000000000000000000000000000000000000000005d21dba0000000000000000000000000000000000000000000000000000000005d02190d97000000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000001400000000000000000000000000000000000000000000000000000000000000160000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000fc0000000000000000000000000000000000000000000000000000de0000b05120c9f93163c99695c6526b799ebca2207fdf7d61addac17f958d2ee523a2206206994597c13d831ec700048dae733300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005c2b97ac9a0000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000000080a06c4eca27a0b86991c6218b36c1d19d4a2e9eb0ce3606eb481111111254eeb25477b68fb85ed929f73a960582000000008b1ccac8";
 const calldataSwap2 = "0x12aa3caf0000000000000000000000003208684f96458c540eb08f6f01b9e9afb2b7d4f0000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec70000000000000000000000003208684f96458c540eb08f6f01b9e9afb2b7d4f0000000000000000000000000a951c184cf0c0f957468a9ab8ec3f76bc75262eb0000000000000000000000000000000000000000000000000000005d19ed03850000000000000000000000000000000000000000000000000000005d0b4f22f0000000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000001400000000000000000000000000000000000000000000000000000000000000160000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002d40000000000000000000000000000000000000000000002b600028800026e00a0c9e75c480000000000000000300200000000000000000000000000000000000000000000000000024000013051004a585e0f7c18e2c414221d6402652d5e0990e5f8a0b86991c6218b36c1d19d4a2e9eb0ce3606eb4800a4a5dcbcdf000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec7000000000000000000000000d0b2f5018b5d22759724af6d4281ac0b132663600000000000000000000000003208684f96458c540eb08f6f01b9e9afb2b7d4f0ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005120d17b3c9784510e33cd5b87b490e79253bcd81e2ea0b86991c6218b36c1d19d4a2e9eb0ce3606eb48004458d30ac9000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec700000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005884993bdd00000000000000000000000000000000000000000000000000000000000000000000000000000000000000003208684f96458c540eb08f6f01b9e9afb2b7d4f0000000000000000000000000000000000000000000000000000000006506b67a0020d6bdbf78dac17f958d2ee523a2206206994597c13d831ec780a06c4eca27dac17f958d2ee523a2206206994597c13d831ec71111111254eeb25477b68fb85ed929f73a9605820000000000000000000000008b1ccac8";
-
-const oneInchApproveData = "0x095ea7b30000000000000000000000001111111254eeb25477b68fb85ed929f73a960582ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
-
+const addressOriginalDODOFlashloanArb = "0xa951c184cf0c0f957468a9ab8ec3f76bc75262eb"; 
+// Helper method to replace the address of the original contract with the address of our contract
 function replaceCalldataAddress(calldata: string, addressOriginalReceiver: string, addressReceiver: string): string {
   const addressOriginalReceiverInCalldata = addressOriginalReceiver.slice(2).toLowerCase();
   const addressReceiverInCalldata = addressReceiver.slice(2).toLowerCase();
@@ -26,10 +39,12 @@ function replaceCalldataAddress(calldata: string, addressOriginalReceiver: strin
   return calldata.replace(addressOriginalReceiverInCalldata, addressReceiverInCalldata);
 }
 
+// This is the calldata to approve 1inch to spend ERC20 tokens
+const oneInchApproveData = "0x095ea7b30000000000000000000000001111111254eeb25477b68fb85ed929f73a960582ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
+
 const flashLoanAmountUSDT = "400000000000"; // 400.000 USDT
 
 describe("🚩 Challenge N: Description", function () {
-  // Change to name and type of your contract
   let dodoFlashloanArb: DODOFlashloanArb;
 
   describe("DODOFlashloanArb", function () {
@@ -44,23 +59,15 @@ describe("🚩 Challenge N: Description", function () {
       contractArtifact = "contracts/DODOFlashloanArb.sol:DODOFlashloanArb";
     }
 
-    it("DODO pool has enough founds ", async function () {
+    it("Executing flahsloan aritrage ", async function () {
       // Get the current block number
       const blockNumber = await ethers.provider.getBlockNumber();
       console.log("Current block number:", blockNumber);
 
-      //const usdtContract = await ethers.getContractAt("IERC20", addressUSDT);
-      //const balance = await usdtContract.balanceOf(addressDODOPool);
-      //console.log("USDT balance:", balance.toString());
-
-      //const [owner] = await ethers.getSigners();
       const dodoFlashloanArbFactory = await ethers.getContractFactory(contractArtifact);
       dodoFlashloanArb = (await dodoFlashloanArbFactory.deploy()) as DODOFlashloanArb;
-      //console.log("\t", " 🛰  Contract deployed on", await yourContract.getAddress());
 
       const addressDODOFlashloanArb = await dodoFlashloanArb.getAddress();
-      
-      //await dodoFlashloanArb.getERC20Balance(addressUSDT, addressDODOPool);
 
       await dodoFlashloanArb.anyThing(addressUSDT, 0, oneInchApproveData);
       await dodoFlashloanArb.anyThing(addressUSDC, 0, oneInchApproveData);
